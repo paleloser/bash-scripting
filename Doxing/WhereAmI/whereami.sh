222831c8417c4d72e3adfb04b2515ca6fc8093f9
paleloser <pablo.caraballorente@gmail.com>
2018-03-12 23:24:08 +0100

    Added -a (duplicate) and -r (replace) options

--- Doxing/WhereAmI/whereami.sh
+++ Doxing/WhereAmI/whereami.sh
@@ -1,68 +1,98 @@
 #!/bin/bash
 
 #       CURRENT DATE
-DATE=`date '+%Y-%m-%d %H:%M:%S'`
-
+DATE=`date '+%Y-%m-%d'`
 #       PUBLIC IP ADDR
 PUBLIC_ADDR=`curl ifconfig.me`
+
+FILENAME="$DATE@$PUBLIC_ADDR"
 cd logs
-#       WHOIS ABOUT OUR NETWORK
-touch "$DATE@$PUBLIC_ADDR"
-echo "---------------------------------------------------------" >> "$DATE@$PUBLIC_ADDR"
-echo "----------------------    WHOIS    ----------------------" >> "$DATE@$PUBLIC_ADDR"
-echo "---------------------------------------------------------" >> "$DATE@$PUBLIC_ADDR"
-whois $PUBLIC_ADDR >> "$DATE@$PUBLIC_ADDR"
 
-#       THANKS HAK5 FOR SHOWING ME WHAT AWK AND CUT DO
-ETHERNET_INTERFACE="eno1"
-WIFI_INTERFACE="wlp7s0"
-LOCAL_NETWORK_ETHERNET_ADDR=$(ip addr show | grep $ETHERNET_INTERFACE | grep inet | awk '{print $2}' | cut -d "/" -f 1)
-LOCAL_NETWORK_ETHERNET_MASK=$(ip addr show | grep $ETHERNET_INTERFACE | grep inet | awk '{print $2}' | cut -d "/" -f 2)
-LOCAL_NETWORK_WIFI_ADDR=$(ip addr show | grep $WIFI_INTERFACE | grep inet | awk '{print $2}' | cut -d "/" -f 1)
-LOCAL_NETWORK_WIFI_MASK=$(ip addr show | grep $WIFI_INTERFACE | grep inet | awk '{print $2}' | cut -d "/" -f 2)
+#	FUCK IT, THIS IS EASIER
+LOGS=$(ls -l | awk '{print $9}')
 
-echo "-------------------------------------------------------------------------------" >> "$DATE@$PUBLIC_ADDR"
-echo "----------------------    LOCAL DISCOVER SCAN (NMAP)    ----------------------" >> "$DATE@$PUBLIC_ADDR"
-echo "-------------------------------------------------------------------------------" >> "$DATE@$PUBLIC_ADDR"
-#	FIRST WE DO A QUICK DISCOVER SCAN (JUST IN CASE WE RAN OUT OF TIME)
+function writeover {
+	touch "$FILENAME"
 
-if [ "$LOCAL_NETWORK_ETHERNET_ADDR" != "127.0.0.1" ];
-then
-        echo "" >> "$DATE@$PUBLIC_ADDR"
-        echo ">> SCANNING ETHERNET NETWORK" >> "$DATE@$PUBLIC_ADDR"
-        sudo nmap -sP $LOCAL_NETWORK_ETHERNET_ADDR/$LOCAL_NETWORK_ETHERNET_MASK >> "$DATE@$PUBLIC_ADDR"
-elif [ "$LOCAL_NETWORK_WIFI_ADDR" != "127.0.0.1" ];
-then
-        echo "" >> "$DATE@$PUBLIC_ADDR"
-        echo ">> SCANNING WIFI NETWORK" >> "$DATE@$PUBLIC_ADDR"
-        sudo nmap -sP $LOCAL_NETWORK_WIFI_ADDR/$LOCAL_NETWORK_WIFI_MASK >> "$DATE@$PUBLIC_ADDR"
-else
-        echo "" >> "$DATE@$PUBLIC_ADDR"
-        echo ">> COULDN'T FIND NETWORK, ANYWAY THEESE ARE BOTH WIFI AND ETHERNET SCANS"
-        sudo nmap -sP $LOCAL_NETWORK_ETHERNET_ADDR/$LOCAL_NETWORK_ETHERNET_MASK >> "$DATE@$PUBLIC_ADDR"
-        sudo nmap -sP $LOCAL_NETWORK_WIFI_ADDR/$LOCAL_NETWORK_WIFI_MASK >> "$DATE@$PUBLIC_ADDR"
-fi
+	#       WHOIS ABOUT OUR NETWORK
+	echo "---------------------------------------------------------" >> "$FILENAME"
+	echo "----------------------    WHOIS    ----------------------" >> "$FILENAME"
+	echo "---------------------------------------------------------" >> "$FILENAME"
+	whois $PUBLIC_ADDR >> "$FILENAME"
 
-echo "" >> "$DATE@$PUBLIC_ADDR"
-echo "-------------------------------------------------------------------------------" >> "$DATE@$PUBLIC_ADDR"
-echo "----------------------    LOCAL AGRESSIVE SCAN (NMAP)    ----------------------" >> "$DATE@$PUBLIC_ADDR"
-echo "-------------------------------------------------------------------------------" >> "$DATE@$PUBLIC_ADDR"
-#	THEN WE LEAVE THE SCRIPT RUNNING AN AGRESSIVE SCAN THAT MAY OR MAY NOT BE FINNISHED
+	#       THANKS HAK5 FOR SHOWING ME WHAT AWK AND CUT DO
+	ETHERNET_INTERFACE="eno1"
+	WIFI_INTERFACE="wlp7s0"
+	LOCAL_NETWORK_ETHERNET_ADDR=$(ip addr show | grep $ETHERNET_INTERFACE | grep inet | awk '{print $2}' | cut -d "/" -f 1)
+	LOCAL_NETWORK_ETHERNET_MASK=$(ip addr show | grep $ETHERNET_INTERFACE | grep inet | awk '{print $2}' | cut -d "/" -f 2)
+	LOCAL_NETWORK_WIFI_ADDR=$(ip addr show | grep $WIFI_INTERFACE | grep inet | awk '{print $2}' | cut -d "/" -f 1)
+	LOCAL_NETWORK_WIFI_MASK=$(ip addr show | grep $WIFI_INTERFACE | grep inet | awk '{print $2}' | cut -d "/" -f 2)
 
-if [ "$LOCAL_NETWORK_ETHERNET_ADDR" != "127.0.0.1" ];
-then
-	echo "" >> "$DATE@$PUBLIC_ADDR"
-	echo ">> SCANNING ETHERNET NETWORK" >> "$DATE@$PUBLIC_ADDR"
-	nmap -A $LOCAL_NETWORK_ETHERNET_ADDR/$LOCAL_NETWORK_ETHERNET_MASK >> "$DATE@$PUBLIC_ADDR"
-elif [ "$LOCAL_NETWORK_WIFI_ADDR" != "127.0.0.1" ];
-then
-	echo "" >> "$DATE@$PUBLIC_ADDR"
-	echo ">> SCANNING WIFI NETWORK" >> "$DATE@$PUBLIC_ADDR"
-	nmap -A $LOCAL_NETWORK_WIFI_ADDR/$LOCAL_NETWORK_WIFI_MASK >> "$DATE@$PUBLIC_ADDR"
-else
-	echo "" >> "$DATE@$PUBLIC_ADDR"
-	echo ">> COULDN'T FIND NETWORK, ANYWAY THEESE ARE BOTH WIFI AND ETHERNET SCANS"
-	nmap -A $LOCAL_NETWORK_ETHERNET_ADDR/$LOCAL_NETWORK_ETHERNET_MASK >> "$DATE@$PUBLIC_ADDR"
-	nmap -A $LOCAL_NETWORK_WIFI_ADDR/$LOCAL_NETWORK_WIFI_MASK >> "$DATE@$PUBLIC_ADDR"
-fi
-cd ..
+	echo "-------------------------------------------------------------------------------" >> "$FILENAME"
+	echo "----------------------    LOCAL DISCOVER SCAN (NMAP)    ----------------------" >> "$FILENAME"
+	echo "-------------------------------------------------------------------------------" >> "$FILENAME"
+	#	FIRST WE DO A QUICK DISCOVER SCAN (JUST IN CASE WE RAN OUT OF TIME)
+
+	if [ "$LOCAL_NETWORK_ETHERNET_ADDR" != "127.0.0.1" ];
+	then
+		echo "" >> "$FILENAME"
+		echo ">> SCANNING ETHERNET NETWORK" >> "$FILENAME"
+		sudo nmap -sP $LOCAL_NETWORK_ETHERNET_ADDR/$LOCAL_NETWORK_ETHERNET_MASK >> "$FILENAME"
+	elif [ "$LOCAL_NETWORK_WIFI_ADDR" != "127.0.0.1" ];
+	then
+		echo "" >> "$FILENAME"
+		echo ">> SCANNING WIFI NETWORK" >> "$FILENAME"
+		sudo nmap -sP $LOCAL_NETWORK_WIFI_ADDR/$LOCAL_NETWORK_WIFI_MASK >> "$FILENAME"
+	else
+		echo "" >> "$FILENAME"
+		echo ">> COULDN'T FIND NETWORK, ANYWAY THEESE ARE BOTH WIFI AND ETHERNET SCANS"
+		sudo nmap -sP $LOCAL_NETWORK_ETHERNET_ADDR/$LOCAL_NETWORK_ETHERNET_MASK >> "$FILENAME"
+		sudo nmap -sP $LOCAL_NETWORK_WIFI_ADDR/$LOCAL_NETWORK_WIFI_MASK >> "$FILENAME"
+	fi
+
+	echo "-------------------------------------------------------------------------------" >> "$FILENAME"
+	echo "----------------------    LOCAL AGRESSIVE SCAN (NMAP)    ----------------------" >> "$FILENAME"
+	echo "-------------------------------------------------------------------------------" >> "$FILENAME"
+	#	THEN WE LEAVE THE SCRIPT RUNNING AN AGRESSIVE SCAN THAT MAY OR MAY NOT BE FINNISHED
+
+	if [ "$LOCAL_NETWORK_ETHERNET_ADDR" != "127.0.0.1" ];
+	then
+		echo "" >> "$FILENAME"
+		echo ">> SCANNING ETHERNET NETWORK" >> "$FILENAME"
+		nmap -A $LOCAL_NETWORK_ETHERNET_ADDR/$LOCAL_NETWORK_ETHERNET_MASK >> "$FILENAME"
+	elif [ "$LOCAL_NETWORK_WIFI_ADDR" != "127.0.0.1" ];
+	then
+		echo "" >> "$FILENAME"
+		echo ">> SCANNING WIFI NETWORK" >> "$FILENAME"
+		nmap -A $LOCAL_NETWORK_WIFI_ADDR/$LOCAL_NETWORK_WIFI_MASK >> "$FILENAME"
+	else
+		echo "" >> "$FILENAME"
+		echo ">> COULDN'T FIND NETWORK, ANYWAY THEESE ARE BOTH WIFI AND ETHERNET SCANS"
+		nmap -A $LOCAL_NETWORK_ETHERNET_ADDR/$LOCAL_NETWORK_ETHERNET_MASK >> "$FILENAME"
+		nmap -A $LOCAL_NETWORK_WIFI_ADDR/$LOCAL_NETWORK_WIFI_MASK >> "$FILENAME"
+	fi
+	cd ..
+	exit
+}
+
+for i in $LOGS; do
+	if [ $i == "$FILENAME" ]; 
+	then
+		if [ "$1" == "-r" ];
+		then
+			echo "REPLACING TODAY'S SCAN"
+			sudo rm -r "$FILENAME"
+			writeover
+		elif [ "$1" == "-a" ];
+		then
+			echo "GENERATING A DUPLICATED SCAN"
+			FILENAME+=" (D)"
+			writeover
+		fi
+		echo "TODAY'S SCAN ALREADY DONE, SKIPPING"		
+		exit
+	fi
+done
+
+writeover
+
